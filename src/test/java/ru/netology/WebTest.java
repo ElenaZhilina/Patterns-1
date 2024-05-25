771d84cd02007600515468693f1e2483d51451ee
Elena <elenazhilina@yandex.ru>
2024-05-25 22:34:16 +0500

    code added

--- src/test/java/ru/netology/WebTest.java
+++ src/test/java/ru/netology/WebTest.java
@@ -1,27 +1,39 @@
 package ru.netology;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.shadow.com.univocity.parsers.common.DataProcessingException;
-import com.codeborne.selenide.SelenideElement;
-
+import org.openqa.selenium.Keys;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import static com.codeborne.selenide.Condition.disappear;
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static java.nio.channels.FileChannel.open;
+import static com.codeborne.selenide.Selenide.*;
+
 
 class WebTest {
     @Test
-    void webTest() {
+    void cardTest() {
         open("http://localhost:9999");
         $("[data-test-id='city']").setValue("Москва");
 
+
+        LocalDate currentDate = LocalDate.now();
+        LocalDate futureDate = currentDate.plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+
+        $x("//*[text()='Дата встречи']").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
+        $x("//*[text()='Дата встречи']").sendKeys(futureDate.format(formatter));
+
         $("[data-test-id='name']").setValue("Елена");
         $("[data-test-id='phone']").setValue("+77772100609");
-        $("[data-test-id='agreement']").click();
+        $("data-test-id=agreement]").click();
+        $("button").click();
 
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
-    }
+        $("#spin_visible").should(disappear, Duration.ofSeconds(15));
 
-    private DataProcessingException $(String s) {
+        $(".notification__title").shouldHave(exactText("Успешно!"));
+
     }
 }
+
