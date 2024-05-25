331c6625b30ba2283efb63a62147af66359c2d9d
Elena <elenazhilina@yandex.ru>
2024-05-25 22:52:52 +0500

    code corrected

--- src/test/java/ru/netology/WebTest.java
+++ src/test/java/ru/netology/WebTest.java
@@ -1,38 +1,38 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import static com.codeborne.selenide.Condition.disappear;
+
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 
-class WebTest {
+public class WebTest {
+
+    LocalDate futureDate = LocalDate.now().plusDays(3);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     @Test
-    void cardTest() {
+    public void cardTest() {
         open("http://localhost:9999");
-        $("[data-test-id='city']").setValue("Москва");
+        $("[data-test-id='city'] input").setValue("Москва");
 
-
-        LocalDate currentDate = LocalDate.now();
-        LocalDate futureDate = currentDate.plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
-
-        $x("//*[text()='Дата встречи']").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
-        $x("//*[text()='Дата встречи']").sendKeys(futureDate.format(formatter));
+        $("[data-test-id='date'] input").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(futureDate.format(formatter));
 
         $("[data-test-id='name']").setValue("Елена");
         $("[data-test-id='phone']").setValue("+77772100609");
         $("data-test-id=agreement]").click();
         $("button").click();
 
-        $("#spin_visible").should(disappear, Duration.ofSeconds(15));
-
-        $(".notification__title").shouldHave(exactText("Успешно!"));
+        $(".notification__title")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Успешно!"));
 
     }
 }
