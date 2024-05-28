f55e7370f966287037efc6f9d08b48ce139320fa
Elena <elenazhilina@yandex.ru>
2024-05-28 20:17:58 +0500

    Initial commit

--- src/test/java/ru/netology/WebTest.java
+++ src/test/java/ru/netology/WebTest.java
@@ -1,19 +1,27 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class WebTest {
+
+    private Faker faker;
+
+    @BeforeEach
+    void setUpAll() {
+        faker = new Faker(new Locale("ru"));
+    }
 
     LocalDate futureDate = LocalDate.now().plusDays(3);
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
@@ -22,18 +30,16 @@ public class WebTest {
     public void cardTest() {
 
         open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Москва");
-
+        String city = faker.address().city();
+        $("[data-test-id='city'] input").setValue(city);
         $("[data-test-id='date'] input").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
         $("[data-test-id='date'] input").sendKeys(futureDate.format(formatter));
-
-        $("[data-test-id='name'] input").setValue("Елена");
-        $("[data-test-id='phone'] input").setValue("+77772100609");
+        String name = faker.name().fullName();
+        $("[data-test-id='name'] input").setValue(name);
+        String phone = faker.phoneNumber().phoneNumber();
+        $("[data-test-id='phone'] input").setValue(phone);
         $("[data-test-id=agreement]").click();
         $$("button").find(Condition.exactText("Забронировать")).click();
-        //$$("button").find(exactText("Забронировать")).shouldBe(Condition.visible, Duration.ofSeconds(15));
-        //$(".button__icon").shouldBe(Condition.visible, Duration.ofSeconds(15));
-        //$("[data-test-id='notification']").shouldHave(exactText("Встреча успешно забронирована на " + futureDate.format(formatter)));
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + futureDate.format(formatter)));
