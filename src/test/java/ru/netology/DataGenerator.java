d6b4a63d98a10db79acf71f00bc97b0400c0c576
Elena <elenazhilina@yandex.ru>
2024-05-30 19:03:43 +0500

    DataGenerator modified

--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -5,26 +5,34 @@ import com.github.javafaker.Faker;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Random;
 
 public class DataGenerator {
-
-    private DataGenerator;
-    public static final Faker faker = new Faker(new Locale("ru"));
-    public static String generateCity() {
-        return faker.address().city();
-    }
-
-    public static String generateName() {
-        return faker.name().fullName();
-    }
-
-    public static String generatePhone() {
-        return faker.phoneNumber().phoneNumber();
+    private DataGenerator() {
     }
 
     public static String generateDate(int daysToAdd) {
         return LocalDate.now().plusDays(daysToAdd).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
+
+        //public static final Faker faker = new Faker(new Locale("ru"));
+        public static String generateCity () {
+        var cities = new String[] {"Москва", "Уфа", "Липецк", "Ярославль",
+                "Белгород", "Орел", "Астрахань", "Чита", "Магадан", "Казань", "Саратов", "Чебоксары",
+                "Хабаровскк", "Мурманск", "Санкт-Петербург"};
+            return cities[new Random().nextInt(cities.length)];
+        }
+
+        public static String generateName () {
+            return faker.name().fullName();
+        }
+
+        public static String generatePhone () {
+            return faker.phoneNumber().phoneNumber();
+        }
+
+
+
     public static class Registration {
         public static User generateUser() {
             Faker localeFaker = new Faker(new Locale("ru"));
@@ -38,4 +46,4 @@ public class DataGenerator {
     }
 }
 
-}
+    }
